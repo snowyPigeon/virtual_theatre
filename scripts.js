a833150697e6c51825c536af21a5de3080bf2316
Snowy Pigeon <7503186+snowyPigeon@users.noreply.github.com>
2021-03-21 11:01:50 +0000

    Added project files to newly created repository

---
+++ scripts.js
@@ -0,0 +1,32 @@
+/* pull and draw curtains */
+/* let curtainsPulled = false;
+function pull_curtains() {
+	if (curtainsPulled == false){
+	document.getElementById("curtain_left").innerHTML = "Pulling curtains back.";
+	document.getElementById("curtain_left").style.width="15px";
+	document.getElementById("curtain_right").innerHTML = "Pulling curtains back.";
+	document.getElementById("curtain_right").style.width="15px";
+	curtainsPulled = true;
+	}
+	else {
+		document.getElementById("curtain_left").innerHTML = "Drawing curtains across.";
+		document.getElementById("curtain_left").style.width="165px";
+		document.getElementById("curtain_right").innerHTML = "Drawing curtains across.";
+		document.getElementById("curtain_right").style.width="165px";
+		curtainsPulled = false;
+	}
+} */
+
+/* duplicate seats */
+/* $(document).ready(function(){
+	$(document).load(function(){
+	$(#central_stalls).clone().appendTo(#stalls);})
+}); */
+
+/* onclick labels */
+/* $(document).ready(function(){
+  $("#orchestral_pit").click(function(){
+    $("#op_label").toggle();
+  });
+}); */
+
